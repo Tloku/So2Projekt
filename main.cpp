a8ecf1e8e39bf7cd65b54985f74b6aae5bf8f388
tloku <tlokinski.dominik1@gmail.com>
2022-05-22 15:02:16 +0200

    Finished stage 2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <list>
 #include <algorithm>
 #include <mutex>
+#include <condition_variable>
 
 struct Box;
 void start(Ball *ball, Box* myBox);
@@ -14,6 +15,7 @@ void startBox(Box* myBox);
 void drawBoard(WINDOW* playWin, Box* myBox);
 void ballCollisionWithRectangle(Ball *ball, Box *myBox);
 void detectKeyPressAndCloseApp(WINDOW* win);
+bool isInSquare(Ball ball, Box *myBox);
 
 struct Box {
     int y1, x1, length;
@@ -24,7 +26,7 @@ struct Box {
 
 // kulki wpadają do prostokąta i w nim się odbijają 3 razy, po 3 odbicu wychodzą z prostokąta,
 // gdy kulka jest w prostokącie to prostokąt się nie porusza
-
+std::condition_variable cv;
 std::list<Ball> balls;
 bool quit = false;
 std::mutex boxCollisionMutex;
@@ -79,6 +81,9 @@ void drawBoard(WINDOW* playWin, Box* myBox) {
         }
 
         for(Ball ball : balls) {
+            if(ball.getBounces() >= ball.getMaxBounces()){
+                continue;
+            }
             mvwaddch(playWin, ball.getY(), ball.getX(), ball.getModel());
         }
         for(int j = 0; j < myBox->length; j++) {
@@ -118,6 +123,21 @@ void start(Ball *ball, Box* myBox) {
         } else {
             ball->moveDown();
         }
+
+        bool check = false;
+        for(Ball ballToCheck: balls) {
+            if(isInSquare(ballToCheck, myBox)) {
+                check = true;
+                break;
+            }
+        }
+
+        if(check/*isInSquare(ball, myBox)*/) {
+            myBox->box_moving = false;
+        } else {
+            cv.notify_one();
+        }
+
         ballCollisionWithRectangle(ball, myBox);
         std::this_thread::sleep_for(std::chrono::microseconds((int)600000 / ball->getSpeed()));
     }
@@ -128,92 +148,74 @@ void ballCollisionWithRectangle(Ball *ball, Box *myBox) {
     if(ball->goingLeft()) {
         if(ball->getX() <= myBox->x1 + 1 && ball->getX() <= myBox->x1 + myBox->length &&
            ball->getY() >= myBox->y1 && ball->getY() <= myBox->y1 + myBox->length && ball->getBouncesInside() < 3) {
-
             ball->setIsGoingLeft(false);
             ball->setBouncesInside(ball->getBouncesInside() + 1);
-            ball->setIsInside(true);
-            ball->setModel('i');
-        } else if (ball->getBouncesInside() == 3){
-            ball->setIsInside(false);
-            ball->setModel('n');
         }
     } else if(!ball->goingLeft()) {
         if(ball->getX() >= myBox->x1 +myBox->length - 1 && ball->getX() < myBox->x1 +myBox->length &&
            ball->getY() >= myBox->y1 && ball->getY() <= myBox->y1 + myBox->length  && ball->getBouncesInside() < 3) {
-
             ball->setIsGoingLeft(true);
             ball->setBouncesInside(ball->getBouncesInside() + 1);
-            ball->setIsInside(true);
-            ball->setModel('i');
-        } else if (ball->getBouncesInside() == 3){
-            ball->setIsInside(false);
-            ball->setModel('n');
         }
     }
     if(ball->goingUp()) {
         if(ball->getY() <= myBox->y1 && ball->getY() <= myBox->y1 + myBox->length &&
            ball->getX() >= myBox->x1 && ball->getX() <= myBox->x1 + myBox->length  && ball->getBouncesInside() < 3) {
-
             ball->setIsGoingUp(false);
             ball->setBouncesInside(ball->getBouncesInside() + 1);
-            ball->setIsInside(true);
-            ball->setModel('i');
-        } else if (ball->getBouncesInside() == 3){
-            ball->setIsInside(false);
-            ball->setModel('n');
         }
 
     } else if (!ball->goingUp()) {
         if(ball->getY() >= myBox->y1 + myBox->length -1 && ball->getY() >= myBox->y1  &&
            ball->getX() >= myBox->x1 && ball->getX() <= myBox->x1 + myBox->length  && ball->getBouncesInside() < 3) {
-
             ball->setIsGoingUp(true);
             ball->setBouncesInside(ball->getBouncesInside() + 1);
-            ball->setIsInside(true);
-            ball->setModel('i');
-        } else if (ball->getBouncesInside() == 3){
-            ball->setIsInside(false);
-            ball->setModel('n');
         }
     }
-    boxCollisionMutex.lock();
-    int counter = 0;
-    for(Ball ballChecker : balls) {
-        if(ballChecker.isBallInside()) {
-            counter++;
-            break;
-        }
+
+}
+
+bool isInSquare(Ball ball, Box *myBox) {
+    if(ball.getY() >= myBox->y1 && ball.getY() < myBox->y1 + myBox->length &&
+        ball.getX() >= myBox->x1 && ball.getX() < myBox->x1 + myBox->length &&
+        ball.getBouncesInside() <= 3) {
+        ball.setModel('i');
+        return true;
     }
-    if(counter) {
-        myBox->box_moving = false;
-    } else {
-        myBox->box_moving = true;
-    }
-    boxCollisionMutex.unlock();
+    return false;
 }
 
 void startBox(Box* myBox)
 {
+    static std::mutex mutex;
     int speedOfBox = rand() % 10 + 1;
     while(true) {
         if(quit) {
             break;
         }
-        if(myBox->box_moving) {
-            if(myBox->goingDown) {
-                myBox->y1++;
-                if(myBox->y1 + myBox->length >= getmaxy(myBox->window) - 1) {
-                    myBox->goingDown = false;
-                    speedOfBox = rand() % 10 + 1;
-                }
-            } else if (!myBox->goingDown) {
-                myBox->y1--;
-                if(myBox->y1 <= 0) {
-                    myBox->goingDown = true;
-                    speedOfBox = rand() % 10 + 1;
-                }
+
+        if(myBox->goingDown) {
+            myBox->y1++;
+            if(myBox->y1 + myBox->length >= getmaxy(myBox->window) - 1) {
+                myBox->goingDown = false;
+                speedOfBox = rand() % 10 + 1;
             }
-            std::this_thread::sleep_for(std::chrono::microseconds((int)1200000 / speedOfBox));
+        } else if (!myBox->goingDown) {
+            myBox->y1--;
+            if(myBox->y1 <= 0) {
+                myBox->goingDown = true;
+                speedOfBox = rand() % 10 + 1;
+            }
         }
+
+        {
+            std::unique_lock<std::mutex> ul(mutex);
+
+            if (!myBox->box_moving) {
+                cv.wait(ul);
+                myBox->box_moving = true;
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::microseconds((int)1200000 / speedOfBox));
     }
 }
